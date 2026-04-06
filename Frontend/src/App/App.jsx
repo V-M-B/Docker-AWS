85bdf662fc39959e11ccbb4866c3f8d011a03c91
V-M-B <vmbvarun@gmail.com>
2026-04-06 23:09:04 +0530

    chore: update Tailwind CSS integration and dependencies
    - Add @tailwindcss/postcss to devDependencies in package.json and package-lock.json
    - Remove individual Tailwind CSS imports in App.css and replace with a single import
    - Update Vite configuration to remove Tailwind CSS plugin
    - Change background color class in App.jsx from 'bg-grey' to 'bg-gray-950'

--- Frontend/src/App/App.jsx
+++ Frontend/src/App/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
   return (
     <main>
-      <div className="h-screen w-full bg-grey"></div>
+      <div className="h-screen w-full bg-gray-950 "></div>
     </main>
   )
 }
